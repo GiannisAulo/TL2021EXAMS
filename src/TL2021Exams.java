b41389d549c2187560280f6ef3564774ba22b1f5
GiannisAulo <johnaulogiaris@gmail.com>
2021-02-12 13:36:22 +0200

    Test Commit

---
+++ src/TL2021Exams.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+
+public class TL2021Exams {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Opel");
+        cars.add("Mazda");
+        System.out.println(cars);
+    }
+
+}
